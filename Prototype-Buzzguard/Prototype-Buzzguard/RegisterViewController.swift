ccdd425761cd4be04124728e539a90902c4ce6fe
WrLai <41589796+WrLai@users.noreply.github.com>
2021-04-18 21:04:38 -0700

    Add files via upload

---
+++ Prototype-Buzzguard/Prototype-Buzzguard/RegisterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewController.swift
+//  Prototype-Buzzguard
+//
+//  Created by Weiren Lai on 4/6/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Prototype-Buzzguard/Prototype-Buzzguard/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Prototype-Buzzguard
+//
+//  Created by Weiren Lai on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
